a9edbae32f70dafc30bd9dd866396479ff6e09bb
UO225811 <UO225811@uniovi.es>
2023-11-24 09:30:45 +0100

    Webpack configuration

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 process.env.NODE_ENV = "development";
 
 module.exports = {
@@ -11,7 +12,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     publicPath: "/",
-    filename: "bundle.js",
+    filename: "bundle.js"
   },
   devServer: {
     stats: "minimal",
@@ -19,28 +20,28 @@ module.exports = {
     historyApiFallback: true,
     disableHostCheck: true,
     headers: { "Access-Control-Allow-Origin": "*" },
-    https: false,
+    https: false
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.API_URL": JSON.stringify("http://localhost:3001"),
+      "process.env.API_URL": JSON.stringify("http://localhost:3001")
     }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
-      favicon: "src/favicon.ico",
-    }),
+      favicon: "src/favicon.ico"
+    })
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"],
+        use: ["babel-loader", "eslint-loader"]
       },
       {
         test: /(\.css)$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 };
